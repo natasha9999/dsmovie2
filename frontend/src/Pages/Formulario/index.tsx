d8824f3f7cc29ddff3d0d1552af3ddd8533f5408
Natasha Alves <alvesna9999@gmail.com>
2022-05-13 16:21:50 -0300

    Layout do formulário

--- frontend/src/Pages/Formulario/index.tsx
+++ frontend/src/Pages/Formulario/index.tsx
@@ -1,6 +1,42 @@
+import './styles.css';
+
 function Formulario() {
+
+    const movie = {
+        id: 1,
+        image: "https://www.themoviedb.org/t/p/w500_and_h282_face/hN3LMCeZDSAsayPwdqob7mjMFOV.jpg",
+        title: "A Anatomia de Grey",
+        count: 2,
+        score: 4.5
+    };
+
     return (
-        <h1>formulario</h1>
+        <div className="dsmovie-form-container">
+            <img className="dsmovie-movie-card-image" src={movie.image} alt={movie.title} />
+            <div className="dsmovie-card-bottom-container">
+                <h3 className="dsmovie-card-title">{movie.title}</h3>
+                <form className="dsmovie-form">
+                    <div className="form-group dsmovie-form-group">
+                        <label htmlFor="email">Informe o seu e-mail</label>
+                        <input type="email" className="form-control" id="email" />
+                    </div>
+                    <div className="form-group dsmovie-form-group">
+                        <label htmlFor="score">Informe a sua avaliação</label>
+                        <select className="form-control" id="score">
+                            <option value="">1</option>
+                            <option value="">2</option>
+                            <option value="">3</option>
+                            <option value="">4</option>
+                            <option value="">5</option>
+                        </select>
+                    </div>
+                    <div className="dsmovie-form-btn-container">
+                        <button type="submit" className="btn btn-primary dsmovie-btn">Salvar</button>
+                    </div>
+                </form>
+                <button className="btn btn-primary dsmovie-btn mt-3">Cancelar</button>
+            </div>
+        </div>
     );
 }
 
