17868229bb3f823b812efe1096dd635f380796c2
Natasha Alves <alvesna9999@gmail.com>
2022-05-15 17:04:26 -0300

    First request

--- frontend/src/Pages/ListaDeFilmes/index.tsx
+++ frontend/src/Pages/ListaDeFilmes/index.tsx
@@ -1,7 +1,14 @@
+import axios from "axios";
 import MovieCard from "Components/MovieCard";
 import Paginacao from "Components/Paginacao";
+import { BASE_URL } from "utils/requests";
 
 function ListaDeFilmes() {
+
+    //FORMA ERRADA
+    axios.get(`${BASE_URL}/movies?size=12&page=1`).then(response => {console.log(response.data)});
+
+
     return (
         <>
             <div className="container">
