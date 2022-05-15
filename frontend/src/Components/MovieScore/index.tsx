157e5724578ea387d1f3bd97996f643da30fffe0
Natasha Alves <alvesna9999@gmail.com>
2022-05-15 19:02:55 -0300

    Show score

--- frontend/src/Components/MovieScore/index.tsx
+++ frontend/src/Components/MovieScore/index.tsx
@@ -1,16 +1,20 @@
 import MovieStars from 'Components/MovieStars';
 import './styles.css';
 
-function MovieScore() {
 
-    const score = 4.5;
-    const count = 9;
+type Props = {
+    score: number;
+    count: number;
+}
+
+
+function MovieScore({ score, count } : Props) {
 
     return (
         <div className="dsmovie-score-container">
             <p className="dsmovie-score-value">{score > 0 ? score.toFixed(1) : '-'}</p>
-            <MovieStars />
-            <p className="dsmovie-score-count">{count}avaliações</p>
+            <MovieStars score={score} />
+            <p className="dsmovie-score-count">{count} avaliações</p>
         </div>
     );
 }
--- frontend/src/Components/MovieStars/index.tsx
+++ frontend/src/Components/MovieStars/index.tsx
@@ -3,14 +3,55 @@ import { ReactComponent as EstrelaVazia } from "assets/img/star-white.svg";
 import { ReactComponent as EstrelaMetade } from "assets/img/star-yellow-white.svg";
 import './styles.css';
 
-function MovieStars() {
+type Props = { score: number; }
+
+type EstrelaProps = { fill: number; }
+
+function getFills(score: number) {
+
+    const fills = [0, 0, 0, 0, 0];
+  
+    const integerPart = Math.floor(score);
+  
+    for (let i = 0; i < integerPart; i++) {
+      fills[i] = 1;
+    }
+  
+    const diff = score - integerPart;
+    if (diff > 0) {
+      fills[integerPart] = 0.5;
+    }
+  
+    return fills;
+  }
+
+
+function Estrela({ fill } : EstrelaProps) {
+    if (fill === 0) {
+        return <EstrelaVazia />
+    }
+
+    else if (fill === 1) {
+        return <EstrelaCheia />
+    }
+
+    else {
+        return <EstrelaMetade />
+    }
+}
+
+
+function MovieStars({ score } : Props) {
+
+    const fills = getFills(score);
+
     return (
         <div className="dsmovie-stars-container">
-            <EstrelaCheia />
-            <EstrelaCheia />
-            <EstrelaCheia />
-            <EstrelaCheia />
-            <EstrelaMetade />
+            <Estrela fill={fills[0]} />
+            <Estrela fill={fills[1]} />
+            <Estrela fill={fills[2]} />
+            <Estrela fill={fills[3]} />
+            <Estrela fill={fills[4]} />
         </div>
     );
 }
