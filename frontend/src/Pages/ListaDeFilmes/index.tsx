3b2c0b795b16291882a59db939e0158c7be3ebe9
Natasha Alves <alvesna9999@gmail.com>
2022-05-15 17:26:48 -0300

    useState, useEffect

--- frontend/src/Pages/ListaDeFilmes/index.tsx
+++ frontend/src/Pages/ListaDeFilmes/index.tsx
@@ -1,16 +1,29 @@
 import axios from "axios";
 import MovieCard from "Components/MovieCard";
 import Paginacao from "Components/Paginacao";
+import { useEffect, useState } from "react";
+import { MoviePage } from "types/movie";
 import { BASE_URL } from "utils/requests";
 
 function ListaDeFilmes() {
 
-    //FORMA ERRADA
-    axios.get(`${BASE_URL}/movies?size=12&page=1`).then(response => {console.log(response.data)});
+    const [pageNumber, setPageNumber] = useState(0);
 
+
+    useEffect(() => {
+        axios.get(`${BASE_URL}/movies?size=12&page=1`).then(response => {
+            const data = response.data as MoviePage;
+            setPageNumber(data.number)});
+    }, []);
 
     return (
         <>
+        <p>{pageNumber}</p>
+
+
+
+
+
             <div className="container">
                 <div className="row">
                     <div className="col-sm-6 col-lg-4 col-xl-3">
@@ -64,6 +77,7 @@ function ListaDeFilmes() {
                  
                 </div>
             </div>
+            <p>{pageNumber}</p>
             <Paginacao />
         </>
     );
